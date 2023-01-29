e1239a9bb517602a563af4447e185879651b3ec8
Luis Oliveira <luis10.pf@gmail.com>
2023-01-29 18:49:33 -0300

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import mockedData from '@/data.json';
+
+const destinations = mockedData.destinations;
+</script>
+
+<template>
+  <div class="home">
+    <h1>All destinations</h1>
+
+    <div class="destinations">
+      <router-link v-for="destination in destinations" :to="destination.slug" :key="destination.id">
+        <h2>{{ destination.name }}</h2>
+        <img :src="`/images/${destination.image}`" :alt="destination.name" />
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
